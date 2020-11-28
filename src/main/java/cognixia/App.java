65276f03b925af90f673f5bb550528c6cb219aca
vishalgujral <vishalgujral@gmail.com>
2020-11-28 16:21:01 +0000

    v1

---
+++ src/main/java/cognixia/App.java
@@ -0,0 +1,13 @@
+package cognixia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
